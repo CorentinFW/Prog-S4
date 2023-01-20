ca74949854c0a038108dfff647f0326204e12b30
CorentinFW <corentinfw@gmail.com>
2023-01-20 09:02:39 +0100

    tout marche

--- java/Java2/src/Main.java
+++ java/Java2/src/Main.java
@@ -1,6 +1,5 @@
-﻿public class ManipulationMain {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
 }
---
+++ java/Java2/src/Manipulation.java
@@ -0,0 +1,5 @@
+public class Manipulation {
+    public static void main(String[] args) {
+        System.out.println("Sucess");
+    }
+}
