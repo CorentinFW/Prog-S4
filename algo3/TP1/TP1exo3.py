7bb4a29a680a22c1e3f4a5c629899c33037eee76
CorentinFW <corentinfw@gmail.com>
2023-02-14 12:55:07 +0100

    bientot fin tp1

--- algo3/TP1/TP1exo3.py
+++ algo3/TP1/TP1exo3.py
@@ -1,5 +1,9 @@
 from dessins import *
 from math import sqrt
+from random import*
+# from random import random
+# from random import uniform
+
 
 def distance(A,B):
     return sqrt((A[0]-B[0])**2+(A[1]-B[1])**2)
@@ -16,5 +20,36 @@ def aretes(P):
     return listeTriplet
 
 
-P = [(6 ,20) ,(67 ,18) ,(96 ,4) ,(32 ,45)]
-print ( aretes ( P ))
+def pointsAleatoires(n,x,y):
+    listeA = []
+    for i in range(n):
+        # listeA.append([round(random.uniform(0.0,float(x)),2),round(random.uniform(0.0,float(y)),2)])
+        listeA.append([random()+randint(0,x),random()+randint(0,y)])
+    return listeA
+
+
+def listesAdjacence(n,A):
+    dico = {}
+
+    for i in range(n):
+        listeVoisin=[]
+        for j in range(len(A)):
+            if i==A[j][1]:
+                listeVoisin.append(A[j][0])
+            elif i==A[j][0]:
+                listeVoisin.append(A[j][1])
+        print(listeVoisin)
+        dico[i] = listeVoisin
+    return dico
+
+
+# A = [(0 ,1) ,(0 ,2) ,(0 ,3) ,(1 ,2) ,(1 ,3) ,(2 ,3)]
+# print ( listesAdjacence (4 , A ))
+
+# P = [(6 ,20) ,(67 ,18) ,(96 ,4) ,(32 ,45)]
+# print ( aretes ( P ))
+
+# print ( pointsAleatoires (3 , 10 , 20))
+
+# dessinPoints(pointsAleatoires(45,10,10))
+dessinGraphe(pointsAleatoires(10,10,10),)
