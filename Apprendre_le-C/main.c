e37a0e35abb1befd1a679cb11a21833fbe39da8f
CorentinFW <corentinfw@gmail.com>
2023-03-03 14:36:37 +0100

    apprendre le c

---
+++ Apprendre_le-C/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int nbrDeFrite;
+    char Frites = 45; 
+    printf("%d\n",Frites);
+    int nbrDePetitPain;
+    signed char lettre = 'A';
+    scanf("%c",&lettre);
+    printf("%c",lettre);
+   
+    printf("%p",&Frites);
+     return 0;
+}
