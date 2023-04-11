054a2df9bb9ea196f53f35bee4b856da6075c245
CorentinFW <corentinfw@gmail.com>
2023-04-11 12:53:15 +0200

    fait calcul pred

--- algo3/TP3/TP3Exo1ChoixCours.py
+++ algo3/TP3/TP3Exo1ChoixCours.py
@@ -16,15 +16,19 @@ def CoursAuHasard(n):
 def TriBullesCours(Cours):
     
     n= 0
-    for j in range(len(Cours)-1)
-        for i in range(1,len(Cours)-1):
+    for j in range(len(Cours)-1):
+        for i in range(0,len(Cours)-1-j):
             if Cours[i][1] >Cours[i+1][1]:
                 Cours[i],Cours[i+1] = Cours[i+1],Cours[i]
     return Cours
 
 
 def CalculPred(n,Cours):
+    Cours = TriBullesCours(Cours)
     Pred=n*[-1]
+    for i in range(n-1):
+        if Cours[i][1]<= Cours[i+1][0]:
+            Pred[i] = Cours[i]
     
     return Pred
 
@@ -65,6 +69,7 @@ choix=int(input("Taper 1 pour un test sur l'exemple donné, 2 pour un ensemble d
 if choix==1:
     Cours=[[76,78,10],[12,17,2],[13,15,1],[19,28,8],[12,20,7],[44,45,9],[43,45,5],[1,8,3]]
     n=8
+    print("Test de calcul Pred = ",CalculPred(n,Cours))
 else:
     n=int(input("Rentrer le nombre de cours voulus: "))
     Cours=CoursAuHasard(n)
@@ -87,3 +92,5 @@ Sol=n*[0]
 print("Calcul d'une solution de valeur maximale d'un choix de cours en prog dyn: ")
 print("Valeur maximale: ", ChoixMaxProgDSol(n,Cours,Pred,Sol))
 print("Solution correspondante :", CalculSolProgDyn(n,Cours,Pred,Sol))
+print("Test de calcul Pred = ",CalculPred(n,Cours))
+print("test de Tri bulle cours = ",TriBullesCours(Cours))
